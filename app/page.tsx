68f198fd0bd895be7979a8849fb4614567497d6b
YashPanwar1408 <panwarhp2351@gmail.com>
2025-04-14 16:37:45 +0530

    Fix: mark homepage as dynamic to resolve static prerender error

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
+
+
 interface HomeProps{
   searchParams:IListingsParams
 }
+
+export const dynamic = "force-dynamic";
 
 const Home = async ({searchParams}:HomeProps)=> {
   const listings = await getListings(searchParams)
